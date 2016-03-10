f08e47eb256d4615856780b4136556a9f49e7ea8
Johnny Ting Shi <johnny.t.shi@gmail.com>
2016-03-09 16:01:29 -0800

    Fuck yeah

---
+++ app/src/test/java/buddybuild/com/helloworldandroid/DummyTest.java
@@ -0,0 +1,18 @@
+package buddybuild.com.helloworldandroid;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DummyTest {
+
+    @Test
+    public void thisWillPass() {
+        assertThat(true, is(true));
+    }
+
+    @Test
+    public void thisShallNotPass() {
+        assertThat(true, is(false));
+    }
+}
